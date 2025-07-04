a002a77aefbf4163e6c8f1c4be996c40d7452185
nakul0405 <142690696+nakul0405@users.noreply.github.com>
2025-07-05 00:19:00 +0530

    Update auth_server.py

--- auth_server.py
+++ auth_server.py
@@ -32,12 +32,14 @@ def home():
 def login():
     user_id = request.args.get('user_id')
     scope = "user-read-currently-playing user-read-recently-played user-top-read"
-    auth_url = "https://accounts.spotify.com/authorize?" + urllib.parse.urlencode({
-        "client_id": SPOTIFY_CLIENT_ID,
-        "response_type": "code",
-        "redirect_uri": SPOTIFY_REDIRECT_URI,
-        "scope": scope,
-        "state": user_id
+    encoded_redirect_uri = urllib.parse.quote(SPOTIFY_REDIRECT_URI, safe='')
+
+auth_url = "https://accounts.spotify.com/authorize?" + urllib.parse.urlencode({
+    "client_id": SPOTIFY_CLIENT_ID,
+    "response_type": "code",
+    "redirect_uri": encoded_redirect_uri,
+    "scope": scope,
+    "state": user_id
     })
     return redirect(auth_url)
 
