73d2ac7f346d6623fb04193ba2b6000e8e77c0cb
nakul0405 <142690696+nakul0405@users.noreply.github.com>
2025-07-04 20:19:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 # config.py
 
-BOT_TOKEN = "8129365197:AAHy31wJxk6_KsIGiyDB7si66xqgFH6BXrQ"
-
-SPOTIFY_CLIENT_ID = "c7e12b52179447bbb6e66aed4ff3e9d0"
-SPOTIFY_CLIENT_SECRET = "f2758294c5a344f689b8e1fec9fa2745"
-SPOTIFY_REDIRECT_URI = "SPOTIFY_REDIRECT_URI = "https://playspotify.onrender.com/callback"
-"
-
-OWNER_ID = 1357641408  # 👑 Tera Telegram ID
+BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+SPOTIFY_CLIENT_ID = "YOUR_SPOTIFY_CLIENT_ID"
+SPOTIFY_CLIENT_SECRET = "YOUR_SPOTIFY_CLIENT_SECRET"
+SPOTIFY_REDIRECT_URI = "https://your-app-name.onrender.com/callback"  # Yeh Render waali URL hogi
+OWNER_ID = 1234567890  # Sirf Render me daalna, GitHub me rakhna optional hai
