9fecaac4793187647cbc2c4cafd1a2e7ffc33e46
nakul0405 <142690696+nakul0405@users.noreply.github.com>
2025-07-05 05:00:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo "🚀 Starting PlaySpotify bot and Flask server..."
+echo "🚀 Starting PlaySpotify bot + Flask..."
 
-# Start Telegram bot
 python3 bot.py &
 
-# Start Flask app
 cd web && python3 app.py
