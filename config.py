ac0c08c9c1aa4567fbbd0356702a38c51f66feec
Nakul Rathod <142690696+nakul0405@users.noreply.github.com>
2025-07-05 15:51:06 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Your Telegram bot token (keep this secret!)
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# Your Flask server base URL (Render domain)
-AUTH_SERVER_URL = os.getenv("AUTH_SERVER_URL", "https://playspotify.onrender.com")
