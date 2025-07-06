4dc8705aeb3e2d5bc508d3403e37900c5d7711de
Nakul Rathod <142690696+nakul0405@users.noreply.github.com>
2025-07-06 14:34:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
+
 #!/bin/bash
-
-echo "✅ Starting Flask Auth Server on port 8000..."
-python3 auth_server.py &
-
-echo "🎧 Starting Telegram Bot..."
-python3 bot.py
+python3 bot.py &
+python3 auth_server.py
