7891021481dc3967696f134bdb5fc0cc798a39f7
Nakul Rathod <142690696+nakul0405@users.noreply.github.com>
2025-07-06 14:25:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-# 👇 This line is VERY important
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
-ADMIN_ID = os.getenv("ADMIN_ID")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
