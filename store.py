c9d5f7b41946282608a3406709d1c23173c8a919
Nakul Rathod <142690696+nakul0405@users.noreply.github.com>
2025-07-06 16:54:51 +0530

    Update store.py

--- store.py
+++ store.py
@@ -1,31 +1,16 @@
 import json
 import os
 
-TOKEN_FILE = "tokens.json"
-COOKIE_FILE = "cookies.json"
-
-def save_token(user_id, data):
-    tokens = load_all(TOKEN_FILE)
-    tokens[str(user_id)] = data
-    save_all(TOKEN_FILE, tokens)
-
-def get_token(user_id):
-    return load_all(TOKEN_FILE).get(str(user_id))
+COOKIES_FILE = "cookies.json"
 
 def save_cookie(user_id, sp_dc):
-    cookies = load_all(COOKIE_FILE)
-    cookies[str(user_id)] = sp_dc
-    save_all(COOKIE_FILE, cookies)
+    if os.path.exists(COOKIES_FILE):
+        with open(COOKIES_FILE, "r") as f:
+            cookies = json.load(f)
+    else:
+        cookies = {}
 
-def get_cookie(user_id):
-    return load_all(COOKIE_FILE).get(str(user_id))
+    cookies[user_id] = sp_dc
 
-def load_all(path):
-    if os.path.exists(path):
-        with open(path, "r") as f:
-            return json.load(f)
-    return {}
-
-def save_all(path, data):
-    with open(path, "w") as f:
-        json.dump(data, f, indent=2)
+    with open(COOKIES_FILE, "w") as f:
+        json.dump(cookies, f)
